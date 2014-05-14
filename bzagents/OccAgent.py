ec222c4a91be6da553e99d0efda8dc1882df8860
Matt <chasing314@gmail.com>
2014-05-13 22:09:55 -0600

    initial calculateProbabilities work

--- bzagents/OccAgent.py
+++ bzagents/OccAgent.py
@@ -3,15 +3,18 @@ import re
 
 class OccAgent(PFAgent):
 	# member constants
-	SENSOR_X = Agent.NOT_SET
-	SENSOR_Y = Agent.NOT_SET
+	SENSOR_X_DIM = Agent.NOT_SET
+	SENSOR_Y_DIM = Agent.NOT_SET
 	GRID_AT_STR = "at"
 	GRID_SIZE_STR = "size"
 	BEGINNING_OCCUPIED_ESTIMATE = .01
+	REPORTED_OBSTACLE_CHAR = "1"
+	CONFIDENT_OF_OBSTACLE = 1
+	CONFIDENT_OF_NO_OBSTACLE = -1
 
 	# member variables
 	probabilities = []
-	probOfObstacles = .5
+	probOfOnes = .1
 
 	####### initialization functions
 	def __init__(self, ip, port):
@@ -34,8 +37,8 @@ class OccAgent(PFAgent):
 		strList = self.getGrid(0)
 		
 		dimensions = strList[1].split("x")
-		SENSOR_X = int(dimensions[0])
-		SENSOR_Y = int(dimensions[1])
+		self.SENSOR_X_DIM = int(dimensions[0])
+		self.SENSOR_Y_DIM = int(dimensions[1])
 
 	####### end initialization functions
 
@@ -45,7 +48,7 @@ class OccAgent(PFAgent):
 		# note that range starts at 2: this loop skips
 		# the 'at' and 'size' lines in the grid list
 		for x in range(2,len(gridList)-2):
-			numOnes += gridList[x].count("1")
+			numOnes += gridList[x].count(self.REPORTED_OBSTACLE_CHAR)
 
 		return numOnes
 
@@ -71,6 +74,39 @@ class OccAgent(PFAgent):
 
 		return strList
 
-	def calculateProbabilities(self, tankNum):
-		gridList = self._getGrid(tankNum)
+	def printProbsToFile(self, filename):
+		world = ""
+		for x in range(0,self.worldHalfSize*2):
+			for y in range(0,self.worldHalfSize*2):
+				world += "{:10.2f}".format(self.probabilities[x][y]) + " "
+			world += "\n"
+
+		outfile = open(filename,'w')
+		print >>outfile, world
+
+	def updateProbabilities(self, tankNum):
+		gridList = self.getGrid(tankNum)
 		beginningPoint = self._getPointFromString(gridList[0]) # bottom-left corner
+		
+		numOnes = 0
+		for x in range(2,2+self.SENSOR_X_DIM):
+			line = gridList[x]
+			#numOnes += line.count(self.REPORTED_OBSTACLE_CHAR)  # count reported 'occupied' samples
+			for y in range(0,self.SENSOR_Y_DIM):
+				believedSOccVal = self.probabilities[x-2][y]
+				if(believedSOccVal == self.CONFIDENT_OF_OBSTACLE or 
+					believedSOccVal == self.CONFIDENT_OF_NO_OBSTACLE):
+					continue
+
+				sampleVal = int(line[y])
+	
+				combinedProbs = 1
+				if(sampleVal == 0): 
+					# represents P(o_i,j = miss | s_i,j = occupied) (false-negative)
+					#combinedProbs *= 
+					a = 5
+				else:	
+					# represents P(o_i,j = hit | s_i,j = occupied) (true-positive) 
+					a = 2
+
+				self.probabilities[x-2][y] = combinedProbs*believedSOccVal
