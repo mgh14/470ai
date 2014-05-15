09ebe788d79d2c91ff47f014aabf2f75af86fc60
dirkamiss <dirkamiss@gmail.com>
2014-05-14 18:18:10 -0600

    Update OccAgent.py
    Added code to update probability that a state is occupied

--- bzagents/OccAgent.py
+++ bzagents/OccAgent.py
@@ -7,11 +7,14 @@ class OccAgent(PFAgent):
 	SENSOR_Y = Agent.NOT_SET
 	GRID_AT_STR = "at"
 	GRID_SIZE_STR = "size"
-	BEGINNING_OCCUPIED_ESTIMATE = .01
+	BEGINNING_OCCUPIED_ESTIMATE = .2
+	TRUE_POSITIVE = .97
+	FALSE_POSITIVE = .03
+	TRUE_NEGATIVE = .9
+	FALSE_NEGATIVE = .1
 
 	# member variables
 	probabilities = []
-	probOfObstacles = .5
 
 	####### initialization functions
 	def __init__(self, ip, port):
@@ -58,7 +61,7 @@ class OccAgent(PFAgent):
 
 		return self.getAdjustedPoint(pointAt)
 
-	def getGrid(self, tankNum):
+	def _getGrid(self, tankNum):
 		raw = self._getRawResponse("occgrid " + str(tankNum) + self.SERVER_DELIMITER)
 		raw = raw[len(self.LIST_START):-1*(len(self.LIST_END) + 1)]  # parse off 'begin\n' and 'end\n'
 		strList = raw.split(self.SERVER_DELIMITER)  # split strings by server delimiter
@@ -74,3 +77,36 @@ class OccAgent(PFAgent):
 	def calculateProbabilities(self, tankNum):
 		gridList = self._getGrid(tankNum)
 		beginningPoint = self._getPointFromString(gridList[0]) # bottom-left corner
+		
+		# iterate through gridList  (remember that the grid needs to be rotated counter-clockwise)
+		for i in range( 0, len(gridList) - 1 ):
+			for j in range( 0, len(gridList[i]) - 1):
+				x = beginningPoint[0] + i
+				y = beginningPoint[1] + j
+				
+				#maybe add checks if x and y are outside world dimensions
+				
+				#If probabilities are above or below a threshhold of probability assume it's correct
+				if self.probabilities[x][y] >= 0.95:
+					self.probabilities[x][y] = 1
+				elif self.probabilities[x][y] <= 0.05:
+					self.probabilities[x][y] = 0
+				else:
+					self.probabilities[x][y] = self.updateProbability( x, y, gridList[x][y] )
+					
+	def updateProbability(self, x, y, observed_value):
+		probOcc = self.NOT_SET
+		probUnOcc = self.NOT_SET
+		if observed_value == 1:
+			#probability that this is an actual occupied space
+			#our probabilities array holds previous probability that it is occupied.
+			probOcc = self.TRUE_POSITIVE * self.probabilities[x][y]
+			# one minus our stored probability is the probability it is unoccupied
+			probUnOcc = self.FALSE_POSITIVE * (1 - self.probabilities[x][y])
+		elif observed_value == 0:
+			#same as above but using our FalseNegative and TrueNegative values.
+			probOcc = self.FALSE_NEGATIVE * self.probabilities[x][y]
+			probUnOcc = self.TRUE_NEGATIVE * (1 - self.probabilities[x][y])
+		
+		return probOcc / (probOcc + probUnOcc)
+		
