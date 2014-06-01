75af0e9852ba8a205c91afda3573f58a258bded4
Matt <chasing314@gmail.com>
2014-05-31 22:50:20 -0600

    add perpendicular movement for placement

--- bzagents/ClayPidgeonAgent.py
+++ bzagents/ClayPidgeonAgent.py
@@ -9,6 +9,7 @@ class ClayPidgeonAgent(Agent):
 		self.tankNum = 0
 		self.velocity = 0.75
 		self.oscillationThreshold = 40	# (in seconds)
+		self.oscillationAngle = math.pi
 
 	def _alignTankWithAngle(self):
 		# desired angle along which the tank will move in a straight line
@@ -17,16 +18,39 @@ class ClayPidgeonAgent(Agent):
 		myTanks = self._query("mytanks")
 		tank0 = myTanks[self.tankNum]
 		angVel = 0.2
-		while (abs(self.getAdjustedAngle(float(tank0[8])) - math.pi) > .005):
+		while (abs(self.getAdjustedAngle(float(tank0[8])) - self.oscillationAngle) > .001):
 			self.commandAgent("angvel " + str(self.tankNum) + " " + str(angVel))
 			tank0 = self._query("mytanks")[self.tankNum]
 		
 		# stop tank rotation
 		self.commandAgent("angvel " + str(self.tankNum) + " 0")
 
+	def _moveToParallelPlane(self):
+		perpAngle = self.getAdjustedAngle(self.oscillationAngle - math.pi/2)
+
+		# rotate to move up
+		angleDiff = abs(self.getMyAngle(self.tankNum) - perpAngle)
+		while(angleDiff > .005):
+			print "diff: " + str(angleDiff) + "; myAng: " + str(self.getMyAngle(self.tankNum))
+			self.setAngularVelocity(0,.3,perpAngle)
+			angleDiff = abs(self.getMyAngle(self.tankNum) - perpAngle)
+
+		self.setAngularVelocity(0,0,perpAngle)
+
+		# move up n units
+		originalPosition = self.getMyPosition(0)
+		position = originalPosition
+		while(position[1] - originalPosition[1] < 100):
+			self.commandAgent("speed " + str(self.tankNum) + " .3")
+			position = self.getMyPosition(0)
+
+		# stop
+		self.stop(0)
+					
+	
 	def play(self):
-		#self._moveToPosition
-		#sys.exit(0)
+		self._moveToParallelPlane()
+		
 		self._alignTankWithAngle()
 
 		velocity = self.velocity
