3e184dfc0873f3130a381315456628927f256730
Matt <chasing314@gmail.com>
2014-06-06 18:59:59 -0600

    example n-gram file

---
+++ speechTag/n-gram.py
@@ -0,0 +1,18 @@
+context = contextconst
+model = {}
+ 
+for word in trainingdata.split():
+    #print (word)
+    model[str(context)] = model.setdefault(str(context),[])+ [word]
+    context = (context+[word])[1:]
+ 
+#print(model)
+ 
+context = contextconst
+for i in range(100):
+    word = random.choice(model[str(context)])
+    print(word,end=" ")
+    context = (context+[word])[1:]
+ 
+print()
+
